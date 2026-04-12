0d70faee8bea6b45d68597ba6f1d61a09d1f03d1
Dhia_Zorai <dhiadhoo5555@gmail.com>
2026-04-12 09:54:06 +0100

    chore: completely remove Electron from project
    - Removed electron dependencies and build configurations from package.json
    - Deleted electron directory (main.js, preload.js, server.js)
    - Removed electron build artifacts from .gitignore
    - Removed standalone prepare script

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Expose protected methods that allow the renderer process to use
-// specific Electron APIs without exposing the entire Electron object
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform,
-  isElectron: true,
-});
